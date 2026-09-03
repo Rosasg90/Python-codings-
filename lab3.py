b208c5489126dcd6b1ca27352533205084386d1b
Rosasg90 <jrosas78909@gmail.com>
2026-09-03 16:42:20 +0000

    work done

---
+++ lab3.py
@@ -0,0 +1,14 @@
+#Josue Rosas 
+#Student ID: 877784637 
+#Section: 08
+#Module 1, Assignmnet 3
+first_integer = int(input("What is the integer?"))
+second_float =float(input("What is the float?"))
+message_str = f"{first_integer} + {second_float}"
+print(message_str)
+message_str = f"{first_integer} - {second_float}"
+print(message_str)
+message_str = f"{first_integer} * {second_float}"
+print(message_str)
+message_str = f"{first_integer} / {second_float}"
+print(message_str)
