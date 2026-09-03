e23372875c3af2a114ecbd9188b3d7e9896a7ccd
Rosasg90 <jrosas78909@gmail.com>
2026-09-03 15:22:42 +0000

    saved work

---
+++ inclasswork.py
@@ -0,0 +1,7 @@
+x = int(11/2)
+print(f"the number is {x ** 4}" )
+
+x,y,z = 1,2,3 
+print (f"x  is {x} and y is {y} and x is {z}")
+name = input ("what is your name?")
+print(f"your name is {name}")
