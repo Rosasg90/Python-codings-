709b0fd764f9212051d78a85af35e6ea34ec8b4e
Rosasg90 <jrosas78909@gmail.com>
2026-09-07 08:08:56 +0000

    rewrote my errors

--- lab3.py
+++ lab3.py
@@ -1,14 +1,22 @@
-#Josue Rosas 
-#Student ID: 877784637 
-#Section: 08
-#Module 1, Assignmnet 3
-first_integer = int(input("What is the integer?"))
-second_float =float(input("What is the float?"))
-message_str = f"{first_integer} + {second_float}"
+# Josue Rosas 
+# Student ID: 877784637 
+# Section: 08
+# Module 1, Asignmnet 3
+first_integer = int(input("What is the integer? "))
+second_float =float(input("What is the float? "))
+
+sum_result = first_integer + second_float
+message_str = f"{first_integer} + {second_float} is {sum_result}"
 print(message_str)
-message_str = f"{first_integer} - {second_float}"
+
+product_result = first_integer - second_float
+message_str = f"{first_integer} - {second_float} is {product_result}"
 print(message_str)
-message_str = f"{first_integer} * {second_float}"
+
+multiper_result = first_integer * second_float
+message_str = f"{first_integer} * {second_float} is {multiper_result}"
 print(message_str)
-message_str = f"{first_integer} / {second_float}"
+
+divide_result = first_integer / second_float 
+message_str = f"{first_integer} / {second_float} is {divide_result}"
 print(message_str)
