46698199b8e97c7ceb87b26b2e9f549ac7437edf
Rosasg90 <jrosas78909@gmail.com>
2026-09-10 20:08:17 +0000

    finished lab

---
+++ a2lab1.py
@@ -0,0 +1,16 @@
+# Josue Rosas 
+# Student ID: 877784637 
+# Section: 08
+# Module 2, Assignment 1
+
+g_list= []
+g_list.append (input(f"What is your favorite game?").title())
+g_list.append (input(f"What is your second favorite game? ").title())
+g_list.append (input(f"What is your third favorite game? ").title())
+
+
+print(f"One of my favorite game is {g_list.pop(0)}")
+print(f"One of my favorite game is {g_list.pop(0)}")
+print(f"One of my favorite game is {g_list.pop(0)}")
+
+
