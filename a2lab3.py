af721aabd90a6ca9635b06f91e8aac0b509cd823
Rosasg90 <jrosas78909@gmail.com>
2026-09-14 18:41:17 +0000

    fixed grammer

--- a2lab3.py
+++ a2lab3.py
@@ -4,14 +4,13 @@
 # Module 2, Assignment 3
 
 uber_list = list(range(100,201,2))
-
-start = int (input (f"what is the start of your slice? "))
-end = int( input (f"what is the end of your slice? "))
+start = int (input (f"What is the start of your slice? "))
+end = int( input (f"What is the end of your slice? "))
 data_list = uber_list[start:end]
 total_int = 0
 for num in data_list:
     total_int += num/ len(data_list)
-print(f"Your slice contain {len(data_list)} values and has a average of {total_int}")
+print(f"Your slice contain {len(data_list)} values and has an average of {total_int}")
 
 
 
