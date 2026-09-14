45dc1735220f950f75c472a713481757cec37e3b
Rosasg90 <jrosas78909@gmail.com>
2026-09-14 18:50:26 +0000

    saved rewrote the code

--- lab4.py
+++ lab4.py
@@ -5,6 +5,6 @@
 
 catchphrase_str = input("Please enter a catchphrase: ")
 print(f"[{catchphrase_str.upper()}]")
-print(f"[{catchphrase_str}]")
+print(f"[{catchphrase_str.lower()}]")
 print(f"[{catchphrase_str.lstrip()}]")
 print(f"[{catchphrase_str.rstrip()}]")
