0c5067e0025f52c43429f6633131a0555f5041c4
Rosasg90 <jrosas78909@gmail.com>
2026-09-15 00:22:23 +0000

    added new to the the sentence

--- a2lab2.py
+++ a2lab2.py
@@ -4,13 +4,13 @@
 # Module 2, Assignment 2
 
 g_list = ['Mortal Kombat', 'Contra', 'Streets Of Rage', 'Shinobi', 'Sonic', 'Phantasy Star']
-print(f"Here are the top Sega games: ")
+print(f"Here are the new top Sega games: ")
 for word in g_list:
     print(word)
 
 user_input = input(f"Which you think should be removed? ")
 g_list.remove(user_input)
 
-print(f"Here are the top Sega games: ")
+print(f"Here are the new top Sega games: ")
 for word in g_list:
     print(word)
