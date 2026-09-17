f23e62a0c2af48adecfff2d8753b05fe1645373a
Rosasg90 <jrosas78909@gmail.com>
2026-09-17 16:45:03 +0000

    finished lab

---
+++ a3lap2.py
@@ -0,0 +1,12 @@
+# Josue Rosas 
+# Student ID: 877784637 
+# Section: 08
+# Module 3, Assignment 2
+
+m_list = ["E.T. the Extra-Terrestrial", "Indiana Jones and the Last Crusade", "Batman", "Rain Man", "Back to the Future Part II"]
+movie = input(f"What is your favorite movie? ")
+if movie in m_list:
+    print(f"Great Choice")
+else:
+    print(f"Never heard of it")
+
